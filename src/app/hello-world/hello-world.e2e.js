19be0a6f05a49e263f6776915f8f127bfebfbb68
Shyam Chen <shyamchen1994@gmail.com>
2016-04-27 22:14:43 +0800

    22nd Commit

--- src/app/hello-world/hello-world.e2e.js
+++ src/app/hello-world/hello-world.e2e.js
@@ -1,9 +1,11 @@
 describe('Hello World', function() {
   beforeEach(function() {
     browser.get('http://localhost:9876/');
+    /*
     browser.wait(function() {
       return browser.executeScript('return !!window.angular');
     }, 5000);
+    */
     element(by.tagName('hello-world')).click();
   });
   it('should have a content', function() {
