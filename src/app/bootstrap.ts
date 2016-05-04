cfa28f2a274d53ece03623276adcc944d8b9a6b3
Shyam Chen <shyamchen1994@gmail.com>
2016-05-04 20:00:48 +0800

    48th Commit

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,4 +1,4 @@
-import { bootstrap } from '@angular/platform-browser';
+import { bootstrap } from '@angular/platform-browser-dynamic';
 import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import { HTTP_PROVIDERS } from '@angular/http';
 import 'rxjs/Rx';
