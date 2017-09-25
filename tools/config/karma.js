402237a2ee773b84d4543158a2d77a4461fce9d5
Shyam Chen <shyamchen1994@gmail.com>
2017-09-25 15:59:44 +0800

    262nd Commit

--- tools/config/karma.js
+++ tools/config/karma.js
@@ -1,23 +1,66 @@
-const { TEST_PORT } = require('../constants');
-const { TEST_CONFIG, SECONDARY_CONFIG } = require('./rollup');
+const { join } = require('path');
+const webpack = require('webpack');
+
+const { SOURCE_ROOT, TEST_PORT } = require('../constants');
 
 module.exports = config => {
   config.set({
     basePath: '../..',
     frameworks: ['jasmine'],
     files: [
-      { pattern: 'src/polyfills.js', watched: false },
-      { pattern: 'src/vendor.js', watched: false },
-      'src/**/*.spec.js'
+      { pattern: 'src/test.ts', watched: false }
     ],
     exclude: [],
     preprocessors: {
-      'src/polyfills.js': ['prerollup'],
-      'src/vendor.js': ['prerollup'],
-      'src/**/*.spec.js': ['rollup']
+      'src/test.ts': ['webpack', 'sourcemap']
     },
-    prerollupPreprocessor: SECONDARY_CONFIG,
-    rollupPreprocessor: TEST_CONFIG,
+    webpack: {
+      devtool: 'inline-source-map',
+      resolve: {
+        extensions: ['.ts', '.js']
+      },
+      module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            loaders: [
+              {
+                loader: 'awesome-typescript-loader',
+                options: { configFileName: join(__dirname, 'tsconfig.spec.json') }
+              },
+              'angular2-template-loader'
+            ]
+          }, {
+            test: /\.html$/,
+            loader: 'html-loader'
+          }, {
+            test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+            loader: 'null-loader'
+          }, {
+            test: /\.css$/,
+            exclude: join(SOURCE_ROOT, 'app'),
+            loader: 'null-loader'
+          }, {
+            test: /\.css$/,
+            include: join(SOURCE_ROOT, 'app'),
+            loader: 'raw-loader'
+          }
+        ]
+      },
+      plugins: [
+        new webpack.ContextReplacementPlugin(
+          /angular(\\|\/)core(\\|\/)@angular/,
+          SOURCE_ROOT,
+          {}
+        )
+      ]
+    },
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
+    webpackServer: {
+      noInfo: true
+    },
     reporters: ['mocha'],
     port: TEST_PORT,
     colors: true,
