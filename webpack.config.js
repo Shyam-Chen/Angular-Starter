4d3de589729a0da5dab26699b019b6601a1f9649
Shyam-Chen <shyamchen1994@gmail.com>
2017-11-07 15:14:55 +0800

    287th Commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const typescript = require('typescript');
-const { AotPlugin } = require('@ngtools/webpack');
+const { AngularCompilerPlugin } = require('@ngtools/webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const rules = [
@@ -57,12 +57,12 @@ const plugins = [
 
 if (process.env.NODE_ENV === 'production') {
   rules.push({
-    test: /\.ts$/,
-    use: ['@ngtools/webpack']
+    test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
+    use: '@ngtools/webpack'
   });
 
   plugins.push(
-    new AotPlugin({
+    new AngularCompilerPlugin({
       tsConfigPath: 'tsconfig.json',
       entryModule: 'src/app/app.module#AppModule'
     }),
