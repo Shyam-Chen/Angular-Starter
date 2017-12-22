a5bfbe70b34c4a18fb8160e1a78aa98bd4f2bc8d
Shyam-Chen <shyamchen1994@gmail.com>
2017-12-22 10:33:21 +0800

    334th Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -67,7 +67,24 @@ import { Location, LocationStrategy, PathLocationStrategy } from '@angular/commo
         </div>
 
         <footer>
-          Learn Angular
+          <div style="width: 50px">
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 250 250" style="enable-background:new 0 0 250 250" xml:space="preserve">
+              <g>
+                <g>
+                  <polygon fill="#fff" points="125,153.4 100.3,153.4 88.6,182.6 88.6,182.6 66.9,182.6 66.8,182.6 125,52.1 125,52.2 125,52.2 125,30 125,30 31.9,63.2 46.1,186.3 125,230 125,230 125,153.4"/>
+                  <polygon fill="#fff" points="108,135.4 125,135.4 125,135.4 125,94.5"/>
+                </g>
+                <g opacity=".9">
+                  <polygon fill="#fff" points="125,153.4 149.7,153.4 161.4,182.6 161.4,182.6 183.1,182.6 183.2,182.6 125,52.1 125,52.2 125,52.2 125,30 125,30 218.1,63.2 203.9,186.3 125,230 125,230 125,153.4"/>
+                  <polygon fill="#fff" points="142,135.4 125,135.4 125,135.4 125,94.5"/>
+                </g>
+              </g>
+            </svg>
+          </div>
+
+          <div>
+            Learn Angular
+          </div>
         </footer>
       </mat-sidenav-content>
     </mat-sidenav-container>
@@ -139,6 +156,8 @@ import { Location, LocationStrategy, PathLocationStrategy } from '@angular/commo
       font-size: 12px;
       background: #3f51b5;
       color: hsla(0,0%,100%,.87);
+      display: flex;
+      flex-direction: row;
     }
   `]
 })
