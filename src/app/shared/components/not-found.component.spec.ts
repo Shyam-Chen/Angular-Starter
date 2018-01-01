76506873f9b63b47961f933f6db54a3d513aaae0
Shyam Chen <shyamchen1994@gmail.com>
2018-01-01 14:41:30 +0800

    356th Commit

--- src/app/shared/components/not-found.component.spec.ts
+++ src/app/shared/components/not-found.component.spec.ts
@@ -7,23 +7,15 @@ import { NotFoundComponent } from './not-found.component';
 describe('NotFoundComponent', () => {
   let fixture: ComponentFixture<NotFoundComponent>;
 
-  let comp: NotFoundComponent;
-  let de: DebugElement;
-  let el: HTMLElement;
-
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [NotFoundComponent]
     });
 
     fixture = TestBed.createComponent(NotFoundComponent);
-
-    comp = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css('h1'));
-    el = de.nativeElement;
   });
 
-  it('should display a text', () => {
-    expect(el.textContent).toContain('404');
+  it('renders markup to snapshot', () => {
+    expect(fixture).toMatchSnapshot();
   });
 });
