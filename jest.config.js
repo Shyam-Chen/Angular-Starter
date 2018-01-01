73db931879b188a3b8b0be8189198e2bfabc3f7d
Shyam Chen <shyamchen1994@gmail.com>
2018-01-01 14:29:41 +0800

    355th Commit

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
-module.exports = {
-  preset: 'jest-preset-angular',
-  setupTestFrameworkScriptFile: '<rootDir>/src/test.ts'
-};
+switch (process.env.TEST_ENV) {
+  case 'app':
+    module.exports = {
+      preset: 'jest-preset-angular',
+      setupTestFrameworkScriptFile: '<rootDir>/src/test.ts'
+    };
+    break;
+  case 'api':
+    module.exports = {};
+    break;
+  default:
+    module.exports = {};
+}
