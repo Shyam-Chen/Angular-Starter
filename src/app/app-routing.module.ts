cdf3b7fce6a12e674926dce90639c5226626f0f4
Shyam Chen <shyamchen1994@gmail.com>
2018-07-23 20:28:59 +0800

    419th Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Location, APP_BASE_HREF, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+
+import { HomeModule } from './home/home.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundModule } from './not-found/not-found.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+export const ROUTES: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'hello-world', loadChildren: './shell/hello-world/hello-world.module#HelloWorldModule' },
+  // { path: 'guide', loadChildren: './shell/guide/guide.module#GuideModule' },
+  { path: '**', component: NotFoundComponent },
+];
+
+@NgModule({
+  imports: [
+    HomeModule,
+    NotFoundModule,
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules }),
+  ],
+  providers: [
+    Location,
+    { provide: APP_BASE_HREF, useValue: '/' },
+    { provide: LocationStrategy, useClass: PathLocationStrategy },
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Location, APP_BASE_HREF, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 
-import { HomeModule } from './home/home.module';
-import { HomeComponent } from './home/home.component';
-import { NotFoundModule } from './not-found/not-found.module';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { SortFilterListModule } from './shared/sort-filter-list.module';
 
 import { AppComponent } from './app.component';
-
-export const ROUTES: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'hello-world', loadChildren: './hello-world/hello-world.module#HelloWorldModule' },
-  { path: '**', component: NotFoundComponent },
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
@@ -30,17 +19,11 @@ export const ROUTES: Routes = [
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
-    HomeModule,
-    NotFoundModule,
     SortFilterListModule,
-    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules }),
+    AppRoutingModule,
   ],
   declarations: [AppComponent],
-  providers: [
-    Location,
-    { provide: APP_BASE_HREF, useValue: '/' },
-    { provide: LocationStrategy, useClass: PathLocationStrategy },
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
