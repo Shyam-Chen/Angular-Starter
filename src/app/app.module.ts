77a33c811eba64e01e3ec69fe2c50d0673d087e0
Shyam Chen <shyamchen1994@gmail.com>
2018-08-02 17:04:53 +0800

    423rd Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 
 import { SortFilterListModule } from './shared/sort-filter-list.module';
@@ -18,6 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
     MatButtonModule,
     MatToolbarModule,
     MatSidenavModule,
+    MatListModule,
     MatIconModule,
     SortFilterListModule,
     AppRoutingModule,
