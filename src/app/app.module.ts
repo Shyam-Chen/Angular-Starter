0f420eb7b22396535d769d95fe1b804e719410ee
Shyam Chen <shyamchen1994@gmail.com>
2018-10-05 17:01:15 +0800

    438th Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
@@ -21,7 +23,9 @@ import { AppComponent } from './app.component';
     MatButtonModule,
     MatToolbarModule,
     MatSidenavModule,
+    MatExpansionModule,
     MatListModule,
+    MatDividerModule,
     MatIconModule,
     NgxsModule.forRoot([]),
     NgxsRouterPluginModule.forRoot(),
