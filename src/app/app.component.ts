f2f25a6ad142c8accfee73dd2094303581638f3b
Shyam Chen <shyamchen1994@gmail.com>
2018-10-10 21:00:13 +0800

    446th Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public state: boolean = false;
   public opened: boolean = !(window.innerWidth < 600);
   public mode: string = window.innerWidth < 600 ? 'over' : 'side';
-
-  click() {
-    this.state = !this.state;
-  }
 }
