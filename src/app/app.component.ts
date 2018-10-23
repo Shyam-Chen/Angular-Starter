f3a03697af4fe58c803c2cc137eb1456a1a752c9
Shyam Chen <shyamchen1994@gmail.com>
2018-10-23 13:49:40 +0800

    450th Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,62 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // TODO: use state
+  public navigation = [
+    {
+      subheader: 'Group 1',
+      icon: 'loyalty',
+      text: 'Link A',
+      route: '',
+      children: [
+        { text: 'Link A-1', route: '' },
+        {
+          text: 'Link A-2',
+          children: [
+            { text: 'Link A-2-1', route: '' },
+            { text: 'Link A-2-2', route: '' },
+          ],
+        },
+      ],
+    },
+    {
+      icon: 'business',
+      text: 'Link B',
+      route: '',
+      children: [
+        { text: 'Link B-1', route: '' },
+        { text: 'Link B-2', route: '' },
+      ],
+    },
+    { icon: 'face', text: 'Link C', route: '' },
+    {
+      subheader: 'Group 2',
+      icon: 'loyalty',
+      text: 'Link D',
+      route: '',
+      children: [
+        { text: 'Link D-1', route: '' },
+        {
+          text: 'Link D-2',
+          children: [
+            { text: 'Link D-2-1', route: '' },
+            { text: 'Link D-2-2', route: '' },
+          ],
+        },
+      ],
+    },
+    {
+      icon: 'business',
+      text: 'Link E',
+      route: '',
+      children: [
+        { text: 'Link E-1', route: '' },
+        { text: 'Link E-2', route: '' },
+      ],
+    },
+    { icon: 'face', text: 'Link F', route: '' },
+  ];
+
   public opened: boolean = !(window.innerWidth < 600);
   public mode: string = window.innerWidth < 600 ? 'over' : 'side';
   public logo = this.sanitizer.bypassSecurityTrustResourceUrl(require('../assets/images/logo.svg'));
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -20,6 +21,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    CommonModule,
     MatButtonModule,
     MatToolbarModule,
     MatSidenavModule,
